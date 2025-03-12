0939ec1b5f5d5b1bce55994b3cc79bda3566fea5
joregesosa <joregesosa@gmail.com>
2025-03-12 10:45:45 -0400

    init calificaciones project

---
+++ src/scripts/calificaciones.js
@@ -0,0 +1,52 @@
+const calificaciones = [
+    {
+      subject: "Matemáticas",
+      icon: "./src/images/math.png",
+      score: 85
+    },
+    {
+      subject: "Ciencias",
+      icon: "./src/images/ciencias.png",
+      score: 92
+    },
+    {
+      subject: "Historia",
+      icon: "️./src/images/historia.png",
+      score: 78
+    },
+    {
+      subject: "Literatura",
+      icon: "./src/images/literatura.png",
+      score: 90
+    },
+    {
+      subject: "Arte",
+      icon: "./src/images/arte.png",
+      score: 88
+    },
+    {
+      subject: "Música",
+      icon: "./src/images/musica.png",
+      score: 95
+    },
+    {
+      subject: "Geografía",
+      icon: "./src/images/geografia.png",
+      score: 82
+    },
+    {
+      subject: "Educación Física",
+      icon: "./src/images/educacion-fisica.png",
+      score: 89
+    },
+    {
+      subject: "Informática",
+      icon: "./src/images/informatica.png",
+      score: 91
+    },
+    {
+      subject: "Idiomas",
+      icon: "️idiomas.png",
+      score: 87
+    }
+  ]
